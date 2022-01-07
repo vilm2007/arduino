f386d6e5b51ac6ed113bc05d717b223bdcb4eafc
vilm2007 <90267228+vilm2007@users.noreply.github.com>
2022-01-07 14:58:33 +0100

    Add files via upload

---
+++ sketch_jan07b/sketch_jan07b.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+hd44780_I2Cexp lcd(0x20, I2Cexp_MCP23008, 7, 6, 5, 4, 3, 2, 1, HIGH);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("IG: vilm2007");
+  lcd.setCursor (4, 1);
+  lcd.print("roppi_hun");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
